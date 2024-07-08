b3af82ed5943cfa3e9d23040c38d0f2259feb06a
Viacheslav <minowaman777@gmail.com>
2024-07-08 15:52:49 +0300

    Refactor controller and added delete

--- university-management/src/main/java/ua/develop/universitymanagement/web/controller/StudentController.java
+++ university-management/src/main/java/ua/develop/universitymanagement/web/controller/StudentController.java
@@ -1,27 +1,16 @@
 package ua.develop.universitymanagement.web.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ua.develop.universitymanagement.model.Student;
-import ua.develop.universitymanagement.service.StudentService;
 
 import java.util.UUID;
 
-@RestController("/api/university")
-@AllArgsConstructor
-public class StudentController {
-    private StudentService service;
+public interface StudentController {
 
-    @PostMapping("/student")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student){
-        return service.saveStudent(student);
-    }
+    public Student createStudent(Student student);
 
-    @GetMapping("/student")
-    @ResponseStatus(HttpStatus.OK)
-    public Student getStudentById(@PathVariable UUID id){
-        return service.getStudentById(id);
-    }
+    public Student getStudentById(UUID id);
+
+    public void deleteStudentById(UUID id);
 }
---
+++ university-management/src/main/java/ua/develop/universitymanagement/web/controller/impl/StudentControllerImpl.java
@@ -0,0 +1,38 @@
+package ua.develop.universitymanagement.web.controller.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ua.develop.universitymanagement.model.Student;
+import ua.develop.universitymanagement.service.StudentService;
+import ua.develop.universitymanagement.web.controller.StudentController;
+
+import java.util.UUID;
+
+@RestController("/api/university")
+@AllArgsConstructor
+public class StudentControllerImpl implements StudentController {
+
+    private StudentService service;
+
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Override
+    public Student createStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+
+    @GetMapping("/student/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Override
+    public Student getStudentById(@PathVariable UUID id) {
+        return service.getStudentById(id);
+    }
+
+    @DeleteMapping("/student/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Override
+    public void deleteStudentById(@PathVariable UUID id) {
+        service.deleteStudentById(id);
+    }
+}

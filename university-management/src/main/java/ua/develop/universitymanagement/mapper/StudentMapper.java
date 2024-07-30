82fe88f7151fed0be458911723fba446b73d458c
Viacheslav <minowaman777@gmail.com>
2024-07-30 15:15:15 +0300

    Added mupstruct and implement StudentMapper

---
+++ university-management/src/main/java/ua/develop/universitymanagement/mapper/StudentMapper.java
@@ -0,0 +1,27 @@
+package ua.develop.universitymanagement.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import ua.develop.universitymanagement.dto.student.StudentRead;
+import ua.develop.universitymanagement.dto.student.StudentSave;
+import ua.develop.universitymanagement.model.Student;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+interface StudentMapper {
+
+    StudentMapper INSTANCE = Mappers.getMapper(StudentMapper.class);
+
+    StudentRead toStudentRead(Student student);
+
+    StudentSave toStudentSave(Student student);
+
+    @Mapping(target = "studentId", ignore = true)
+    Student toStudent(StudentSave studentSave);
+
+    List<Student> toStudentList(List<StudentSave> studentSaveList);
+
+    List<StudentRead> toStudentReadList(List<Student> studentList);
+}

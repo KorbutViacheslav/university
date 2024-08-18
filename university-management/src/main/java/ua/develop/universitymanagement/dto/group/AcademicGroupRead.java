f9d46d2e4cd06b34c5d442a21c341094b03e3c50
Viacheslav <minowaman777@gmail.com>
2024-08-18 18:34:43 +0300

    added validation schema to academic read

--- university-management/src/main/java/ua/develop/universitymanagement/dto/group/AcademicGroupRead.java
+++ university-management/src/main/java/ua/develop/universitymanagement/dto/group/AcademicGroupRead.java
@@ -1,12 +1,16 @@
 package ua.develop.universitymanagement.dto.group;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import ua.develop.universitymanagement.dto.student.StudentRead;
 
 import java.util.Set;
 
-public record AcademicGroupRead(String groupNumber,
+public record AcademicGroupRead(
+        @Schema(description = "Group number", example = "TE58")
+        String groupNumber,
 
-                                String department,
+        @Schema(description = "Department of the group", example = "Transport Engineering")
+        String department,
 
-                                Set<StudentRead> students) {
+        Set<StudentRead> students) {
 }

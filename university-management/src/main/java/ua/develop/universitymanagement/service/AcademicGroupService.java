f9a74b13f6272cd1ef7c03bb41959ae48aab8906
Viacheslav <minowaman777@gmail.com>
2024-08-24 20:16:47 +0300

    Created AcademicGroupService and added to project

---
+++ university-management/src/main/java/ua/develop/universitymanagement/service/AcademicGroupService.java
@@ -0,0 +1,7 @@
+package ua.develop.universitymanagement.service;
+
+import ua.develop.universitymanagement.model.AcademicGroup;
+
+public interface AcademicGroupService {
+    AcademicGroup saveAcademicGroup(AcademicGroup academicGroup);
+}
